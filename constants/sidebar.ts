e8c870029ae523fb40973e71bccb042b5ec328b5
R.Moroizumi <m.ryousuke0401@iclioud.com>
2025-10-30 22:35:03 +0900

    Update sidebar constants to reflect versioning changes in URLs and add CacheSignal entry

--- constants/sidebar.ts
+++ constants/sidebar.ts
@@ -2,11 +2,16 @@ export const sidebar_19_2 = [
   {
     label: 'Activity',
     icon: 'activity',
-    href: '/19-2/activity',
+    href: '/19.2/activity',
   },
   {
     label: 'useEffectEvent',
     icon: 'settings',
-    href: '/19-2/use-effect-event',
+    href: '/19.2/use-effect-event',
+  },
+  {
+    label: 'CacheSignal',
+    icon: 'settings',
+    href: '/19.2/cache-signal',
   },
 ];
